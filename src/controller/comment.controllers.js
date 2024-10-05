16269e45437a05899384e788b09eb2af136bb7e5
Rock6982 <rodrigoqm75@gmail.com>
2024-10-04 22:22:28 -0400

    update "getComments"

--- src/controller/comment.controllers.js
+++ src/controller/comment.controllers.js
@@ -1,9 +1,10 @@
 import Comment from "../models/commentSchema.js";
-
+import Publication from "../models/publicationSchema.js";
 // todos los comentarios, de una publicacion. PERO NOSE COMO DIRIGIRLO AL COMENTARIO,
 export const getComments = async (req, res) => {
   try {
     const comments = await Comment.find().populate("user");
+    // .populate("publication");
     res.status(200).json(comments);
   } catch (error) {
     res.status(500).sendStatus(500);
@@ -16,12 +17,18 @@ export const createComment = async (req, res) => {
   try {
     const com = Comment(req.body);
     await com.save();
+    await Publication.findByIdAndUpdate(
+      req.body.publication, // ID de la publicación
+      { $push: { comments: com._id } }, // Agregar el comentario al array
+      { new: true } // Devuelve la publicación actualizada
+    );
     res.status(201).sendStatus(201);
   } catch (error) {
     res.status(500).sendStatus(500);
     console.log(error);
   }
 };
+
 // borra el comentario,validando antes que sea el usuario que comento
 // export const updateComment = async (req, res) => {
 //   try {
